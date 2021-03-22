38895c859471f63a61bcb5a38f4ccd8caf6c1af1
guilherme2912 <80900287+guilherme2912@users.noreply.github.com>
2021-03-22 20:35:15 -0300

    Adicionado arquivos da primeira aula

---
+++ Dev2aulas/src/br/com/dev2me/Aula1.java
@@ -0,0 +1,20 @@
+package br.com.dev2me;
+
+import javax.swing.JOptionPane;
+
+public class Aula1 {
+
+	public static void main(String[] args) {
+		int n1 = Integer.valueOf(JOptionPane.showInputDialog("Digite seu número 1"));
+		int n2 = Integer.valueOf(JOptionPane.showInputDialog("Digite seu número 2"));
+		
+		int soma = 0;
+		
+		soma = n1 + n2;
+		
+		System.out.println("O valor é: " + soma);
+		
+		
+	}
+
+}
