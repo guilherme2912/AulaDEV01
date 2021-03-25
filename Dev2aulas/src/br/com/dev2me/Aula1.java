5e0cc68705c09d9c4724e1301821b1fed163a7f6
guilherme2912 <guilherme2912@hotmail.com>
2021-03-25 19:19:03 -0300

    Atualização de arquivos 25/03/2021

--- Dev2aulas/src/br/com/dev2me/Aula1.java
+++ Dev2aulas/src/br/com/dev2me/Aula1.java
@@ -5,16 +5,19 @@ import javax.swing.JOptionPane;
 public class Aula1 {
 
 	public static void main(String[] args) {
-		int n1 = Integer.valueOf(JOptionPane.showInputDialog("Digite seu número 1"));
-		int n2 = Integer.valueOf(JOptionPane.showInputDialog("Digite seu número 2"));
+		String nome = JOptionPane.showInputDialog("Meu nome é: ");
+
+		  int n1 = Integer.valueOf(JOptionPane.showInputDialog("Digite o valor da matricula: "));
+		  int n2 = Integer.valueOf(JOptionPane.showInputDialog("Digite o valor da primeira parcela: "));
+		  
+		 int soma = 0;
 		
-		int soma = 0;
-		
+
 		soma = n1 + n2;
 		
-		System.out.print("O valor é: " + soma);
+		System.out.println("Para o estudante: " + nome);
+		System.out.println("O valor desse mês: " + soma);
 		
-		
-	}
 
+	}
 }
